24c2ca87d84385adb551b11284824110d9aa38ea
Tobias Wernig <towernig@edu.aau.at>
2023-03-11 10:20:47 +0100

    added functionality for 2.2

--- app/src/main/java/aau/wernig/se2einzelphase/MainActivity.java
+++ app/src/main/java/aau/wernig/se2einzelphase/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         Button sendButton = findViewById(R.id.buttonSend);
         EditText inputMatNr = findViewById(R.id.textMatNr);
         TextView answerText = findViewById(R.id.txtAnswer);
+        Button calculateButton = findViewById(R.id.buttonCalculate);
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,13 +38,45 @@ public class MainActivity extends AppCompatActivity {
                     throw new RuntimeException(e);
                 }
                 answerText.setText(client.outputText);
+            }
+        });
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                try {
+                    int matNr = Integer.parseInt(inputMatNr.getText().toString());
+                    if(alternierendeQuersummeGerade(matNr)){
+                        answerText.setText("Die alternierende Quersumme ist gerade");
+                    }else{
+                        answerText.setText("Die alternierende Quersumme ist ungerade");
+                    }
+                } catch (NumberFormatException e) {
+                    answerText.setText("Dies ist keine gueltige Matrikelnummer");
+                }
 
 
             }
         });
-
     }
 
+    public boolean alternierendeQuersummeGerade(int matNr){
 
+        int quersumme = 0;
+        int plusminusFaktor = 1;
+        while(matNr > 0){
+            int aktuelleZiffer = matNr%10;
+            quersumme = quersumme + (aktuelleZiffer * plusminusFaktor);
+            matNr = matNr/10;
+            plusminusFaktor = -plusminusFaktor;
+        }
+
+        if(quersumme %2 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
 }
